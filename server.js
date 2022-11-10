a4a9174b0a6f196e54bc0a331d8beb5f637090d2
Patryk Majchrzak <patrykmajchrzakdev@gmail.com>
2022-11-10 19:32:36 +0100

    Added some comments what certain stuff does

--- server.js
+++ server.js
@@ -1,71 +1,104 @@
-const express = require('express')
-const app = express()
-const MongoClient = require('mongodb').MongoClient
-const PORT = 2121
-require('dotenv').config()
-
+const express = require("express");
+const app = express();
+const MongoClient = require("mongodb").MongoClient;
+const PORT = 2121;
+require("dotenv").config();
 
 let db,
-    dbConnectionStr = process.env.DB_STRING,
-    dbName = 'rap'
+  dbConnectionStr = process.env.DB_STRING,
+  //name of a db
+  dbName = "rap";
 
-MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
-    .then(client => {
-        console.log(`Connected to ${dbName} Database`)
-        db = client.db(dbName)
+//connect to db
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true }).then(
+  (client) => {
+    console.log(`Connected to ${dbName} Database`);
+    //and set a db name
+    db = client.db(dbName);
+  }
+);
+
+//What file is going to be served
+app.set("view engine", "ejs");
+
+//Makes folder "public" accessible from any file
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//What happens when i go to main url '/'
+app.get("/", (request, response) => {
+  //get collection of 'rappers', get all documents, push it to the array
+  db.collection("rappers")
+    .find()
+    .sort({ likes: -1 })
+    .toArray()
+    //data is holding an array of objects from db
+    .then((data) => {
+      //responding with ejs file and passing array of objects in variable 'info' to ejs file
+      response.render("index.ejs", { info: data });
     })
-    
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+    .catch((error) => console.error(error));
+});
 
-
-app.get('/',(request, response)=>{
-    db.collection('rappers').find().sort({likes: -1}).toArray()
-    .then(data => {
-        response.render('index.ejs', { info: data })
+//post request
+app.post("/addRapper", (request, response) => {
+  //go to db
+  db.collection("rappers")
+    //add new db document
+    .insertOne({
+      //stuff got from index.ejs inputs
+      stageName: request.body.stageName,
+      birthName: request.body.birthName,
+      //lieks is hardcoded
+      likes: 0,
     })
-    .catch(error => console.error(error))
-})
-
-app.post('/addRapper', (request, response) => {
-    db.collection('rappers').insertOne({stageName: request.body.stageName,
-    birthName: request.body.birthName, likes: 0})
-    .then(result => {
-        console.log('Rapper Added')
-        response.redirect('/')
+    //when stuff is collected and added just refresh to have updated collection on site
+    .then((result) => {
+      console.log("Rapper Added");
+      response.redirect("/");
     })
-    .catch(error => console.error(error))
-})
+    .catch((error) => console.error(error));
+});
 
-app.put('/addOneLike', (request, response) => {
-    db.collection('rappers').updateOne({stageName: request.body.stageNameS, birthName: request.body.birthNameS,likes: request.body.likesS},{
+app.put("/addOneLike", (request, response) => {
+  db.collection("rappers")
+    .updateOne(
+      {
+        stageName: request.body.stageNameS,
+        birthName: request.body.birthNameS,
+        likes: request.body.likesS,
+      },
+      {
         $set: {
-            likes:request.body.likesS + 1
-          }
-    },{
-        sort: {_id: -1},
-        upsert: true
+          likes: request.body.likesS + 1,
+        },
+      },
+      {
+        sort: { _id: -1 },
+        upsert: true,
+      }
+    )
+    .then((result) => {
+      console.log("Added One Like");
+      response.json("Like Added");
     })
-    .then(result => {
-        console.log('Added One Like')
-        response.json('Like Added')
+    .catch((error) => console.error(error));
+});
+
+//request was sent via main.js file (i made a js request)
+app.delete("/deleteRapper", (request, response) => {
+  //go to db
+  db.collection("rappers")
+    //delete passed values in main.js (basically find an object that has a stageNameS clicked item)
+    .deleteOne({ stageName: request.body.stageNameS })
+    .then((result) => {
+      console.log("Rapper Deleted");
+      response.json("Rapper Deleted");
     })
-    .catch(error => console.error(error))
+    .catch((error) => console.error(error));
+});
 
-})
-
-app.delete('/deleteRapper', (request, response) => {
-    db.collection('rappers').deleteOne({stageName: request.body.stageNameS})
-    .then(result => {
-        console.log('Rapper Deleted')
-        response.json('Rapper Deleted')
-    })
-    .catch(error => console.error(error))
-
-})
-
-app.listen(process.env.PORT || PORT, ()=>{
-    console.log(`Server running on port ${PORT}`)
-})
+app.listen(process.env.PORT || PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
